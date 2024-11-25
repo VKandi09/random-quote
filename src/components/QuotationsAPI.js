e8c73e941025a220b5d0c0ccd5acdf05cc912fa1
Varsha Kandi <kandivarsha09@gmail.com>
2024-11-25 11:28:02 -0500

    Initial commit

---
+++ src/components/QuotationsAPI.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+
+export const QuotationsAPI = () => {
+  const [value, setValue] = useState("");
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  async function getQuotes() {
+    try {
+        const response = await fetch('https://api.quotable.io/random'); 
+        const data = await response.json();
+        setValue(data);
+    } catch (error) {
+        console.error('Error fetching quote:', error);
+    }
+  }
+
+  return (
+    <div className="container">
+      <div className="quotes-app">
+        <h1>Quote .</h1>
+        <div className="quote">
+          <i className='bx bxs-quote-alt-left left-quote'></i>
+          <p>{value.content}</p>
+          <p className="text-author">- {value.author}</p>
+          <i className='bx bxs-quote-alt-right right-quote'></i>
+        </div>
+        <div className="circles">
+          <div className="circle-1"></div>
+          <div className="circle-2"></div>
+          <div className="circle-3"></div>
+          <div className="circle-4"></div>
+        </div>
+      <button
+        onClick={getQuotes}
+      >
+        Shuffle
+      </button>
+      </div>
+    </div>
+  );
+};
