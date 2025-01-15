fed5be2ea16eb30687e23e14fceac53247a9972c
Varsha Kandi <kandivarsha09@gmail.com>
2025-01-15 11:58:12 -0500

    Added loading logic

--- src/components/QuotationsAPI.js
+++ src/components/QuotationsAPI.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 
 export const QuotationsAPI = () => {
   const [value, setValue] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     getQuotes();
@@ -9,9 +10,11 @@ export const QuotationsAPI = () => {
 
   async function getQuotes() {
     try {
+        setIsLoading(true);
         const response = await fetch('https://programming-quotes-api.azurewebsites.net/api/quotes/random'); 
         const data = await response.json();
         setValue(data);
+        setIsLoading(false);
     } catch (error) {
         console.error('Error fetching quote:', error);
     }
@@ -23,7 +26,7 @@ export const QuotationsAPI = () => {
         <h1>Quote .</h1>
         <div className="quote">
           <i className='bx bxs-quote-alt-left left-quote'></i>
-          <p>{value.text}</p>
+          <p>{!isLoading ? value.text : "Loading..."}</p>
           <p className="text-author">- {value.author}</p>
           <i className='bx bxs-quote-alt-right right-quote'></i>
         </div>
